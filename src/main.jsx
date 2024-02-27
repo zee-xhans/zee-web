5c487cad3c570053cf6fb639411c3b84bd12742c
unknown <mszxhe@gmail.com>
2024-02-27 10:16:28 +0700

    selesai memuat project

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App';
+import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "profile",
+    element: <h1 className="text-center text-2xl pt-52">name : fulan <br /> school : at bina qurani</h1>,
   },
 ]);
 
